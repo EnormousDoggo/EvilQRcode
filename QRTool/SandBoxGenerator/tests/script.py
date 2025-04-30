c939c8d174d5473a057529e25f9c1de5d60c4722
Enormous Doggo <tgelly@myges.fr>
2025-04-30 09:26:27 +0200

    I'm the god of ansible

--- QRTool/SandBoxGenerator/tests/script.py
+++ QRTool/SandBoxGenerator/tests/script.py
@@ -15,7 +15,7 @@ def run_playbook(playbook_path, inventory_path=None, extravars=None):
 
 # Exemple d'utilisation
 run_playbook(
-    playbook_path='/home/user/Documents/COURS/PA/EvilQRcode/QRTool/SandBoxGenerator/tests/playbook_tests.yml',
-    inventory_path='/home/user/Documents/COURS/PA/EvilQRcode/QRTool/SandBoxGenerator/tests/inventory',
+    playbook_path='/home/gitlab-runner/workspace-ansible/qr-code/tests/playbook_tests.yml',
+    inventory_path='/home/gitlab-runner/workspace-ansible/qr-code/tests/inventory',
     extravars={'url_a_tester': 'https://www.google.com/'}
 )
